7d9be71b247f2f8da86de112574905bfe773c125
Lukas <s20126045@students.duvalschools.org>
2026-04-17 12:32:47 -0400

    Update TextureManager.pytest

--- TextureManager.py
+++ TextureManager.py
@@ -19,6 +19,6 @@ class Textures:
     def PreloadTextures(self):
         self.AddPreloadedTexture(os.path.join("Assets", "Textures", "WallTemp.png"), "Wall")
         self.AddPreloadedTexture(os.path.join("Assets", "Textures", "hud.png"), "UI_Main")
-        self.AddPreloadedTexture(os.path.join("Assets", "Textures", "DB Shotgun icon.png"), "UI_Main")
+
 
 textures = Textures()
